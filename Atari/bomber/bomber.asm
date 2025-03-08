0442f5b8c35462778a54ca903a256b66c3b71fcb
joshuawaheed <joshua.waheed@gmail.com>
2025-03-08 20:52:35 +0000

    Section 13: Random Numbers

--- Atari/bomber/bomber.asm
+++ Atari/bomber/bomber.asm
@@ -21,6 +21,7 @@ JetColorPtr     word         ; pointer to player0 color lookup table
 BomberSpritePtr word         ; pointer to player1 sprite lookup table
 BomberColorPtr  word         ; pointer to player1 color lookup table
 JetAnimOffset   byte         ; player0 frame offset for sprite animation
+Random          byte         ; used to generate random bomber x-position
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Define constants
@@ -40,14 +41,16 @@ Reset:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initialize RAM variables and TIA registers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #68
+    sta JetXPos              ; JetXPos = 68
     lda #10
     sta JetYPos              ; JetYPos = 10
-    lda #60
-    sta JetXPos              ; JetXPos = 60
+    lda #62
+    sta BomberXPos           ; BomberXPos = 62
     lda #83
     sta BomberYPos           ; BomberYPos = 83
-    lda #54
-    sta BomberXPos           ; BomberXPos = 54
+    lda #%11010100
+    sta Random               ; Random = $D4
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initialize the pointers to the correct lookup table adresses
@@ -108,7 +111,7 @@ StartFrame:
     sta VBLANK               ; turn off VBLANK
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Display the 96 visible scanlines of our main game (because 2-line kernel)
+;; Display 192 visible scanlines of our game (96 lines because 2-line kernel)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 GameVisibleLine:
     lda #$84
@@ -168,7 +171,7 @@ GameVisibleLine:
     lda #0
     sta JetAnimOffset        ; reset jet animation frame to zero each frame
 
-    sta WSYNC                ; wait for final scanline
+    sta WSYNC                ; wait for a scanline
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Display Overscan
@@ -229,9 +232,8 @@ UpdateBomberPosition:
     dec BomberYPos           ; else, decrement enemy y-position for next frame
     jmp EndPositionUpdate
 .ResetBomberPosition
-    lda #96
-    sta BomberYPos
-                              ; TODO: set bomber X position to random number
+    jsr GetRandomBomberPos   ; call subroutine for random bomber positio
+
 EndPositionUpdate:           ; fallback for the position update code
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -258,6 +260,38 @@ SetObjectXPos subroutine
     asl                      ; four shift lefts to get only the top 4 bits
     sta HMP0,Y               ; store the fine offset to the correct HMxx
     sta RESP0,Y              ; fix object position in 15-step increment
+    rts
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Subroutine to generate a Linear-Feedback Shift Register random number
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Generate a LFSR random number for the X-position of the bomber.
+;; Divide the random value by 4 to limit the size of the result to match river.
+;; Add 30 to compensate for the left green playfield
+;; The routine also sets the Y-position of the bomber to the top of the screen.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+GetRandomBomberPos subroutine
+    lda Random
+    asl
+    eor Random
+    asl
+    eor Random
+    asl
+    asl
+    eor Random
+    asl
+    rol Random               ; performs a series of shifts and bit operations
+
+    lsr
+    lsr                      ; divide the value by 4 with 2 right shifts
+    sta BomberXPos           ; save it to the variable BomberXPos
+    lda #30
+    adc BomberXPos           ; adds 30 + BomberXPos to compensate for left PF
+    sta BomberXPos           ; and sets the new value to the bomber x-position
+
+    lda #96
+    sta BomberYPos           ; set the y-position to the top of the screen
+
     rts
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
